3ea723a506a6a5ae2d67ee35c472612c28777880
alysonviana <alyson.viana@iesb.edu.br>
2025-08-20 21:34:43 -0300

    Atualizacoes da apredizagem

---
+++ praticas/01-Primeiro-Projeto/index.js
@@ -0,0 +1,16 @@
+console.log("Rodando meu primeiro projeto em Node");
+
+let nome = "ALYSON";
+let sobrenome = "VIANA";
+
+console.log(`${nome} ${sobrenome}`);
+
+let idade = "23 anos";
+
+console.log(`Eu tenho ${idade}`);
+
+let exec = 3.0;
+let trab = 1.0;
+let prova = 6.0;
+
+console.log(`A nota final calculada(${exec+trab+prova}).`)
