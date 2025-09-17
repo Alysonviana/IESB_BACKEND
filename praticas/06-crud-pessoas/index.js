7f19fe64c4fcd2d05900838b3a3aa5702f71d216
alysonviana <alyson.viana@iesb.edu.br>
2025-09-17 20:17:00 -0300

    projet 6

---
+++ praticas/06-crud-pessoas/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+
+// configurar e mapear os intemediarios
+const cors = require('cors')
+app.use(cors()) // habilitar o CORS do brouser
+app.use(express.json()) // receber Json no body da requisição
+
+// mapear os meus routes
+
+
+// executar a aplicação 
+
+app.listen(3000, () => {
+    console.log("Aplicação rodando em https://localhost:3000")
+})
