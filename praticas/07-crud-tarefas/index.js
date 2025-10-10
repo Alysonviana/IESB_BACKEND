45fcac40f1244b3e7a2baff4bd85f0e49c849f82
alysonviana <alyson.viana@iesb.edu.br>
2025-10-09 21:55:34 -0300

    atualizações

--- praticas/07-crud-tarefas/index.js
+++ praticas/07-crud-tarefas/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
+
+const DB_HOST=process.env.DB_HOST
+const DB_USER=process.env.DB_USER
+const DB_PASS=process.env.DB_PASS
+const DB_NAME=process.env.DB_NAME
+
+const url = 'mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0'
 
 const app = express()
 
