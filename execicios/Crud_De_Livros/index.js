1b544d1a08c655e8afb21fe6131750a6a38fe29e
alysonviana <alyson.viana@iesb.edu.br>
2025-10-16 20:55:20 -0300

    Crud_08

---
+++ execicios/Crud_De_Livros/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const DB_HOST= process.env.DB_HOST
+const DB_USER= process.env.DB_USER
+const DB_PASS= process.env.DB_PASS
+const DB_NAME= process.env.DB_NAME
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+const app = express()
+app.use(express.json())
+
+mongoose.connect(url)
+
+.then(()=>{
+    console.log("Conectado Com Sucesso!!!")
+})
+.catch(err => {
+    console.log("Erro ao conecta no mongodb!!", err)
+})
+
+
+
+
+
+app.listen(3000, () =>{
+    console.log("aplicação rodando em http://localhost:3000")
+})
