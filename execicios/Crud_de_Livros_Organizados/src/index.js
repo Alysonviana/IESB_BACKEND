a1055da613b3301a4cb6d78717b036cd8211d5e5
alysonviana <alyson.viana@iesb.edu.br>
2025-10-29 20:55:16 -0300

    atualização

--- execicios/Crud_de_Livros_Organizados/src/index.js
+++ execicios/Crud_de_Livros_Organizados/src/index.js
@@ -8,9 +8,15 @@ const port = 3000;
 
 app.use(express.json());
 
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_NAME = process.env.DB_NAME
+
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 )
+
 .then(() => console.log('MongoDB conectado'))
 .catch(err => console.error('Erro ao conectar MongoDB:', err));
 
